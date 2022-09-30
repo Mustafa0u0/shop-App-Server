5d268b1f7dd55682eab50d30c24dcf142073bafb
mustafa0_0 <mustafa.r2018@gmail.com>
2022-09-30 18:15:49 +0000

    uploud server

---
+++ index.js
@@ -0,0 +1,36 @@
+// Import from packges
+const e = require('express');
+const express = require('express');
+const mongoose = require('mongoose');
+const adminRouter = require('./routes/admin');
+// Import from other files
+const authRouter = require('./routes/auth');
+const productRouter = require('./routes/product');
+const userRouter = require('./routes/user');
+
+
+// Init
+const PORT = 3000;
+const app = express();
+const DB ="mongodb+srv://mustafa:mustafa@cluster0.jb9lj.mongodb.net/?retryWrites=true&w=majority";
+
+// middleware 
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+// Connections
+ mongoose
+ .connect(DB)
+ .then(()=>{
+    console.log('Connection Successful');
+ })
+ .catch(e=>{
+    console.log(e);
+ });
+
+app.listen(PORT ,'0.0.0.0',() => {
+    console.log(`connected at port ${PORT}` );
+});
